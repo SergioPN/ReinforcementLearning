e6f1924366797c38d2f4797f6f89830e314cd8bd
Sergio Perez <wdsspec@gmail.com>
2019-03-19 23:04:53 +0100

    Working version with retarded model

--- cartpole-v0/model1.py
+++ cartpole-v0/model1.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+import gym
 from tensorflow import keras
 from keras.callbacks import TensorBoard
 from Generacion_Partidas import PartidasRandom
@@ -36,20 +37,49 @@ def TrainModel(training_data, model=False):
     mov = to_categorical(mov)
 
     if not model:
+        print("Starting a new model")
         model = BuildModel(input_size = 4)
 
-    tensorboard = TensorBoard(log_dir="logs/{}".format(time())) # tensorboard --logdir=logs
-
+    # tensorboard = TensorBoard(log_dir="logs/{}".format(time())) # tensorboard --logdir=logs
 
     model.fit(obs, mov,
-              epochs = 50,
-              callbacks=[tensorboard])
+              epochs = 5,
+              batch_size = 128,
+              callbacks=[])
 
     return model
 
 
-partidas_train = PartidasRandom(num_partidas = 5000)
+partidas_train = PartidasRandom(num_partidas = 100000, score_minimo = 70)
 
+len(partidas_train)
 model = TrainModel(partidas_train)
 
-pwd
+
+model.save("./models/test1")
+
+
+
+def PlayModel(model):
+    env = gym.make('CartPole-v0')
+    record = 0
+    for _ in range(100):
+        done = False
+        observation = env.reset()
+        env.render()
+        current_points = 0
+        for i in range(500):
+            action = np.argmax(model.predict(observation.reshape((1,4))))
+            # if action==1:
+            #     action = 0
+            # else:
+            #     action = 1
+            observation, reward, done, info = env.step(action)
+            current_points += 1
+            env.render()
+
+            if done:
+                if current_points > record:
+                    print(f"Record with {current_points}")
+                    record = current_points
+                break
