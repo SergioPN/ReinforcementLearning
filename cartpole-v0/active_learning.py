3fa40169153c38cffbdfb238a7e08d1d51544413
Sergio Perez <wdsspec@gmail.com>
2019-04-30 19:32:48 +0200

    Defining an Agent v0.01

--- cartpole-v0/active_learning.py
+++ cartpole-v0/active_learning.py
@@ -36,36 +36,55 @@ def BuildModel(input_size, outputs = 2):
 
     return model
 
+
+class Agent(input_size, outputs = 2):
+    def __init__(self):
+        pass
+
+    def Policy(state):
+        """
+        Este es el que dado un estado devuelve una accion
+        Policy(a|s)=A
+        """
+
+    def ValueFunction(state):
+        """
+        Este es el encargado de encontrar un policy que maximice el reward
+        V(s) = E(R)
+        """
+
+
+
 # %%
 
 model = BuildModel(4)
 
-env = gym.make('CartPole-v0')
-observation = env.reset()
-env.render()
+enviroment = gym.make('CartPole-v0')
+state = enviroment.reset()
+# enviroment.render()
 score = 0
 record = 0
 games = 0
 while games < 1000:
-    predict = model.predict(observation.reshape(1,4))
+    predict = model.predict(state.reshape(1,4))
     action = np.argmax(predict)
-    new_observation, reward, done, info = env.step(action)
+    new_state, reward, done, info = enviroment.step(action)
     score += reward
-    new_observation = new_observation.reshape(1,4)
-    reward = to_categorical(reward, num_classes = 2).reshape(1,2).astype(int)
-    history = model.fit(new_observation,
-                        reward,
+    new_state = new_state.reshape(1,4)
+    action = to_categorical(action, num_classes = 2).reshape(1,2).astype(int)
+    history = model.fit(state,
+                        np.round(predict),
                         verbose = 0,
                         shuffle = False)
-    observation = new_observation
-    env.render()
+    state = new_state
+    # enviroment.render()
     # print(history.history['loss'], end = "")
     if done:
         if score > record:
             print("Record: ", score)
             record = score
-        observation = env.reset()
+        state = enviroment.reset()
         score = 0
         games += 1
 
-# env.close()
+# enviroment.close()
